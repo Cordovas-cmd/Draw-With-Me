c7609222bad30e03c342f780e08989cb80ce0c5a
samuel cordova <cordovas1337@gmail.com>
2022-10-20 19:49:19 -0500

    added some functionality to get a line on the canvas will improve upon later

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext('2d');
 
 let size = 20;
 let color = 'black';
+let x;
+let y;
 
 function drawCircle(x, y) {
 
@@ -13,5 +15,13 @@ function drawCircle(x, y) {
 }
 
 
-
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2,y2)
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+    ctx.stroke()
+}
 drawCircle(100, 200)
+drawLine(300, 300, 300, 500)
