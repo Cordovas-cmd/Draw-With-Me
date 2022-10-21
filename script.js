245588fc02e2e693546318515aafe046cd2d7074
samuel cordova <cordovas1337@gmail.com>
2022-10-20 20:07:13 -0500

    added the event listeners and code for mouse up and down

--- script.js
+++ script.js
@@ -2,9 +2,28 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 20;
+let isPressed = false;
 let color = 'black';
 let x;
 let y;
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
+
+    x = e.offsetX
+    y = e.offsetY
+
+    console.log(isPressed, x, y)
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false
+
+    x = undefined;
+    y = undefined;
+
+    console.log(isPressed, x, y)
+})
 
 function drawCircle(x, y) {
 
